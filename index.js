b3026a9994b74a427f58aa96a81cbc30d7188a61
Alex Lu <alex.luyuezhou@gmail.com>
2020-03-11 00:01:30 -0700

    authenticate added not working yet

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose")
+    mongoose = require("mongoose"),
+    User = require("./models/user"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
     // methodOverride = require("method-override"),
     // Cat = require("./models/cat"),
     // User = require("./models/user"),
     // Img = require("./models/image"),
     // fs = require("fs")
 
-var catRoutes = require("./routes/catRoutes");
-// 	userRoutes = require("./routes/userRouts"),
-// 	indexRoutes = require("./routes/index")
+var catRoutes = require("./routes/catRoutes"),
+    // 	userRoutes = require("./routes/userRouts"),
+    indexRoutes = require("./routes/indexRoutes")
 
 //if (process.env.NODE_ENV === 'goorm') {
 
@@ -45,12 +49,36 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 // app.use(methodOverride("_method"));
 
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Yiheng is my son",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    //res.locals.error = req.flash("error");
+    //res.locals.success = req.flash("success");
+    next();
+});
+
+
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 
+app.use("/", indexRoutes);
 app.use("/cats", catRoutes);
+
 
 
 //Tell Express to listen to requests. Start server.
---
+++ routes/indexRoutes.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+
+router.get("/", function(req, res) {
+    //console.log("User: " + req.user);
+    res.render("landing");
+});
+
+//==============
+//AUTH ROUTES
+//==============
+
+//show sign up form
+router.get("/register", function(req, res) {
+    res.render("register");
+});
+
+//handle signup logic
+router.post("/register", function(req, res) {
+    //req.body.username
+    //req.body.password
+    User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            //req.flash("error", err.message)
+            return res.redirect("/register");
+        }
+
+        //log the user in
+        passport.authenticate("local")(req, res, function() {
+            //req.flash("success", "Welcome to YelpCamp, " + user.username);
+            res.redirect("/");
+        });
+    });
+});
+
+//LOGIN ROUTES
+router.get("/login", function(req, res) {
+    res.render("login");
+});
+
+//handle login logic
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+}), function(req, res) {});
+
+
+//LOGOUT ROUTE
+router.get("/logout", function(req, res) {
+    req.logout();
+    //req.flash("success", "Logged You Out!")
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next) {
+    console.log(req.isAuthenticated());
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect("/login");
+}
+
+module.exports = router;
