46196482a1b64c0747f6c1c96ab4582e1262528b
Alex Lu <alex.luyuezhou@gmail.com>
2020-03-16 01:22:45 -0700

    ??

--- index.js
+++ index.js
@@ -83,6 +83,6 @@ app.use("/cats/:id/posts", postingRoutes);
 // });
 
 
-app.listen(3000 || process.env.PORT, process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Cloud CPEN391 has started!!")
 });
